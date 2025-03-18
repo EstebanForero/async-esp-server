6005b34d0206f04aab0c4d6b597dfe69d40da263
Esteban Forero <estebanmff@outlook.com>
2025-03-18 18:33:00 -0500

    Added escential tasks

--- src/peripheral_tasks.rs
+++ src/peripheral_tasks.rs
@@ -1,2 +1,21 @@
+use embassy_sync::blocking_mutex::raw::{CriticalSectionRawMutex, NoopRawMutex};
+use embassy_sync::signal::Signal;
 
-static TEMP_SIGNAL: Sig
+static TEMP_SIGNAL: Signal<CriticalSectionRawMutex, f64> = Signal::new();
+static GAS_SIGNAL: Signal<CriticalSectionRawMutex, f64> = Signal::new();
+static FLAME_SIGNAL: Signal<CriticalSectionRawMutex, f64> = Signal::new();
+
+#[embassy_executor::task]
+async fn temp_reader_task() {}
+
+#[embassy_executor::task]
+async fn gas_reader_task() {}
+
+#[embassy_executor::task]
+async fn flame_reader_task() {}
+
+#[embassy_executor::task]
+async fn display_task() {}
+
+#[embassy_executor::task]
+async fn alarms_task() {}
