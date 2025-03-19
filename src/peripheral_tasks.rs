b67fe7468d4a62fb7adcb7f769b029d84914dc54
Esteban Forero <estebanmff@outlook.com>
2025-03-18 19:11:25 -0500

    Update tasks

--- src/peripheral_tasks.rs
+++ src/peripheral_tasks.rs
@@ -1,18 +1,16 @@
 use embassy_sync::blocking_mutex::raw::{CriticalSectionRawMutex, NoopRawMutex};
 use embassy_sync::signal::Signal;
 
-static TEMP_SIGNAL: Signal<CriticalSectionRawMutex, f64> = Signal::new();
-static GAS_SIGNAL: Signal<CriticalSectionRawMutex, f64> = Signal::new();
-static FLAME_SIGNAL: Signal<CriticalSectionRawMutex, f64> = Signal::new();
+struct SensorValues {
+    temp: f64,
+    gas: u32,
+    flame: bool,
+}
+
+static SENSOR_VALS_SIGNAL: Signal<CriticalSectionRawMutex, SensorValues> = Signal::new();
 
 #[embassy_executor::task]
-pub async fn temp_reader_task() {}
-
-#[embassy_executor::task]
-pub async fn gas_reader_task() {}
-
-#[embassy_executor::task]
-pub async fn flame_reader_task() {}
+pub async fn sensor_reader_task() {}
 
 #[embassy_executor::task]
 pub async fn display_task() {}
