0fccbe43cb9ec0195e3bb6580c91fef77915490a
Esteban Forero <estebanmff@outlook.com>
2025-03-18 19:08:32 -0500

    public

--- src/peripheral_tasks.rs
+++ src/peripheral_tasks.rs
@@ -6,16 +6,16 @@ static GAS_SIGNAL: Signal<CriticalSectionRawMutex, f64> = Signal::new();
 static FLAME_SIGNAL: Signal<CriticalSectionRawMutex, f64> = Signal::new();
 
 #[embassy_executor::task]
-async fn temp_reader_task() {}
+pub async fn temp_reader_task() {}
 
 #[embassy_executor::task]
-async fn gas_reader_task() {}
+pub async fn gas_reader_task() {}
 
 #[embassy_executor::task]
-async fn flame_reader_task() {}
+pub async fn flame_reader_task() {}
 
 #[embassy_executor::task]
-async fn display_task() {}
+pub async fn display_task() {}
 
 #[embassy_executor::task]
-async fn alarms_task() {}
+pub async fn alarms_task() {}
