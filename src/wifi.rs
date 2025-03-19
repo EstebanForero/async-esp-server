2bb7161eb7bcc6d7483d85cf6a209ae9932b16f4
Joel-109 <joelalac@unisabana.edu.co>
2025-03-18 20:23:39 -0500

    Wifi2

--- src/wifi.rs
+++ src/wifi.rs
@@ -9,8 +9,8 @@ use esp_wifi::EspWifiController;
 
 use crate::mk_static;
 
-const SSID: &str = "iPhone (61)";
-const PASSWORD: &str = "Joel1234";
+const SSID: &str = "AHIKZA 5G";
+const PASSWORD: &str = "HKZ993";
 
 #[embassy_executor::task]
 async fn connection_task(mut controller: WifiController<'static>) {
