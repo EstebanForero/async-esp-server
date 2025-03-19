82487c26fc34503732034fec92d79b306d881122
Esteban Forero <estebanmff@outlook.com>
2025-03-19 09:18:36 -0500

    Gas sensor created

--- src/gas_sensor.rs
+++ src/gas_sensor.rs
@@ -1,19 +1,42 @@
 use esp_hal::{
-    analog::adc::{Adc, AdcConfig},
-    gpio::GpioPin,
+    analog::adc::{Adc, AdcConfig, AdcPin},
+    gpio::{AnyPin, GpioPin},
     peripherals::ADC1,
+    Blocking,
 };
+use esp_println::println;
 
-struct GasSensor {}
+pub struct GasSensor<'a> {
+    //pin: GpioPin<34>,
+    adc: Adc<'a, ADC1, Blocking>,
+    analog_pin: AdcPin<GpioPin<34>, ADC1>,
+}
 
-impl GasSensor {
-    fn new(adc: ADC1, pin: GpioPin<34>) -> Self {
+impl<'a> GasSensor<'_> {
+    pub fn new(adc: ADC1, pin: GpioPin<34>) -> Self {
         let mut adc_config = AdcConfig::default();
 
-        let mut pin = adc_config.enable_pin(pin, esp_hal::analog::adc::Attenuation::_11dB);
+        let analog_pin = adc_config.enable_pin(pin, esp_hal::analog::adc::Attenuation::_11dB);
 
-        let mut adc = Adc::new(adc, adc_config);
+        let adc = Adc::new(adc, adc_config);
 
-        todo!()
+        Self { adc, analog_pin }
+    }
+
+    pub fn get_value(&mut self) -> u16 {
+        let value;
+        loop {
+            let val_err = self.adc.read_oneshot(&mut self.analog_pin);
+
+            if let Err(err) = val_err {
+                println!("Error in gas sensor get_value {err:?}");
+                continue;
+            }
+
+            value = val_err.unwrap();
+            break;
+        }
+
+        value
     }
 }
