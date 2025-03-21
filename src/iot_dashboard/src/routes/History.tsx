efb9da3e3284b02ef700f09c99587753fc161e6a
Esteban Forero <estebanmff@outlook.com>
2025-03-20 19:01:53 -0500

    Code working, stress testing

--- src/iot_dashboard/src/routes/History.tsx
+++ src/iot_dashboard/src/routes/History.tsx
@@ -1,4 +1,4 @@
-import { createResource, onCleanup } from "solid-js";
+import { createResource, ErrorBoundary, onCleanup, resetErrorBoundaries } from "solid-js";
 import { fetchSensorValuesHistory, ValueHistoryArray } from "../backend/backend";
 
 
@@ -13,7 +13,9 @@ const History = (props: Props) => {
     if (!historyData.loading) {
       console.log("Fetching sensor history data...");
       refetch();
+
     }
+    resetErrorBoundaries()
   }, props.sensorRefetchRate);
 
   onCleanup(() => {
@@ -21,33 +23,35 @@ const History = (props: Props) => {
   });
 
   return (
-    <div class="history-container">
-      <h2>Historical Values</h2>
-      <section class="sensor-history">
-        <h3>Temperature History</h3>
-        <ul>
-          {historyData()?.values?.reverse().map((val) => (
-            <li>{val.temp.toFixed(2)} °C</li>
-          )) ?? "Loading..."}
-        </ul>
-      </section>
-      <section class="sensor-history">
-        <h3>Gas History</h3>
-        <ul>
-          {historyData()?.values?.reverse().map((val) => (
-            <li>{val.gas.toString()} ppm</li>
-          )) ?? "Loading..."}
-        </ul>
-      </section>
-      <section class="sensor-history">
-        <h3>Flame History</h3>
-        <ul>
-          {historyData()?.values?.reverse().map((val) => (
-            <li>{val.flame ? "Detected" : "Not Detected"}</li>
-          )) ?? "Loading..."}
-        </ul>
-      </section>
-    </div>
+    <ErrorBoundary fallback={<div>Error...</div>}>
+      <div class="history-container">
+        <h2>Historical Values</h2>
+        <section class="sensor-history">
+          <h3>Temperature History</h3>
+          <ul>
+            {historyData()?.values?.reverse().map((val) => (
+              <li>{val.temp.toFixed(2)} °C</li>
+            )) ?? "Loading..."}
+          </ul>
+        </section>
+        <section class="sensor-history">
+          <h3>Gas History</h3>
+          <ul>
+            {historyData()?.values?.reverse().map((val) => (
+              <li>{val.gas.toString()} ppm</li>
+            )) ?? "Loading..."}
+          </ul>
+        </section>
+        <section class="sensor-history">
+          <h3>Flame History</h3>
+          <ul>
+            {historyData()?.values?.reverse().map((val) => (
+              <li>{val.flame ? "Detected" : "Not Detected"}</li>
+            )) ?? "Loading..."}
+          </ul>
+        </section>
+      </div>
+    </ErrorBoundary>
   );
 };
 
